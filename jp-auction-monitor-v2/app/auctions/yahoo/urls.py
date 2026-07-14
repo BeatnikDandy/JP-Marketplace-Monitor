7e3759568f1b1364c7b389769a2491842d6331a5
Gabriel Veras <143140005+BeatnikDandy@users.noreply.github.com>
2026-07-14 20:46:26 +0000

    Remove credenciais bancos locais e copia antiga do repositorio

--- jp-auction-monitor-v2/app/auctions/yahoo/urls.py
+++
@@ -1,9 +0,0 @@
-BASE_URL = "https://auctions.yahoo.co.jp/search/search"
-
-
-def search_url(keyword):
-
-    return (
-        f"{BASE_URL}"
-        f"?p={keyword}"
-    )
--- jp-auction-monitor-v2/app/database/__init__.py
+++
@@ -1,9 +0,0 @@
-from .db import Base, engine
-from . import models
-
-
-def init_database():
-
-    Base.metadata.create_all(
-        bind=engine
-    )
